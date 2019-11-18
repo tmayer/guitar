15d07cfda273b085cd627acfc2e14f3d8a5ad357
Thomas Mayer <thomas.mayer@holidaycheck.com>
2019-11-18 14:10:50 +0100

    Include interactivity when hovering over fret numbers and BEAD/GCF

--- fretboard/draw.js
+++ fretboard/draw.js
@@ -15,6 +15,56 @@ var noteColors = {"c": "red",
                   "a": "orange",
                   "b": "sienna"}
 
+var notesPerFret = {
+    0: [["e", 3], ["a", 3], ["d", 4], ["g", 4], ["b", 4], ["e", 5]],
+    1: [["f", 3], ["c", 5], ["f", 5]], 
+    2: [["b", 3], ["e", 4], ["a", 4]],
+    3: [["g", 3], ["c", 4], ["f", 4], ["d", 5], ["g", 5]],
+    4: [["b", 4]],
+    5: [["a", 3], ["d", 4], ["g", 4], ["c", 5], ["e", 5], ["a", 5]],
+    6: [["f", 5]],
+    7: [["b", 3], ["e", 4], ["a", 4], ["d", 5], ["b", 5]],
+    8: [["c", 4], ["f", 4], ["g", 4], ["c", 5]],
+    9: [["b", 4], ["e", 5]],
+   10: [["d", 4], ["g", 4], ["c", 5], ["f", 5], ["a", 5], ["d", 6]],
+   11: [[]],
+   12: [["e", 4], ["a", 4], ["d", 5], ["g", 5], ["b", 5], ["e", 6]],
+   13: [["f", 4], ["c", 6], ["f", 6]], 
+   14: [["b", 4], ["e", 5], ["a", 5]],
+   15: [["g", 4], ["c", 5], ["f", 5], ["d", 6], ["g", 6]],
+   16: [["b", 5]],
+   17: [["a", 4], ["d", 5], ["g", 5], ["c", 6], ["e", 6], ["a", 6]],
+   18: [["f", 6]],
+   19: [["b", 4], ["e", 5], ["a", 5], ["d", 6], ["b", 6]],
+   20: [["c", 5], ["f", 5], ["g", 5], ["c", 6]],
+   21: [["b", 5], ["e", 5]],
+   22: [["d", 5], ["g", 5], ["c", 6], ["f", 6], ["a", 6], ["d", 7]],
+   23: [[]],
+   24: [["e", 5], ["a", 5], ["d", 6], ["g", 6], ["b", 6], ["e", 7]],
+
+};
+
+var notesPerString = {
+    "6:e2": 0, "6:f2": 1, "6:g2": 3, "6:a2": 5, "6:b2": 7, "6:c3": 8,
+    "6:d3": 10, "6:e3": 12, "6:f3": 13, "6:g3": 15, "6:a3": 17, "6:b3": 19,
+    "6:c4": 20, "6:d4": 22, "6:e4": 24,
+    "5:a2": 0, "5:b2": 2, "5:c3": 3, "5:d3": 5, "5:e3": 7, "5:f3": 8,
+    "5:g3": 10, "5:a3": 12, "5:b3": 14, "5:c4": 15, "5:d4": 17, "5:e4": 19,
+    "5:f4": 20, "5:g4": 22, "5:a4": 24,
+    "4:d3": 0, "4:e3": 2, "4:f3": 3, "4:g3": 5, "4:a3": 7, "4:b3": 9,
+    "4:c4": 10, "4:d4": 12, "4:e4": 14, "4:f4": 15, "4:g4": 17, "4:a4": 19,
+    "4:b4": 21, "4:c5": 22, "4:d5": 24,
+    "3:g3": 0, "3:a3": 2, "3:b3": 4, "3:c4": 5, "3:d4": 7, "3:e4": 9, "3:f4": 10,
+    "3:g4": 12, "3:a4": 14, "3:b4": 16, "3:c5": 17, "3:d5": 19, "3:e5": 21,
+    "3:f5": 22, "3:g5": 24,
+    "2:b3": 0, "2:c4": 1, "2:d4": 3, "2:e4": 5, "2:f4": 6, "2:g4": 8, "2:a4": 10,
+    "2:b4": 12, "2:c5": 13, "2:d5": 15, "2:e5": 17, "2:f5": 18, "2:g5": 20,
+    "2:a5": 22, "2:b5": 24,
+    "1:e4": 0, "1:f4": 1, "1:g4": 3, "1:a4": 5, "1:b4": 7, "1:c5": 8,
+    "1:d5": 10, "1:e5": 12, "1:f5": 13, "1:g5": 15, "1:a5": 17, "1:b5": 19,
+    "1:c6": 20, "1:d6": 22, "1:e6": 24
+};
+
 var Scales = {
     // scales
     lydian: "c d e f# g a b",
@@ -153,16 +203,25 @@ function renderNotes(note_s, width, frets){
 
     var notes = [];
 
-    function newAnnotation(text) { return new Vex.Flow.Annotation(text); }
-
     notes_all.forEach(function(note, i){
         let n = {keys: [note[0] + '/' + note[1]], duration: '4'};
+            
 
             let sn = new VF.StaveNote(n);
             if(note_s.length > 0){
-                if((note_s[0][0] != note[0]) || (parseInt(note_s[0][1]) != parseInt(note[1]))){
+                var found = false;
+                note_s.forEach(function(currNote, k){
+                    //console.log('note', note, 'currNote', currNote);
+                    if(((currNote[0] == note[0]) && (parseInt(currNote[1]) == parseInt(note[1])))){
+                        //console.log("found", currNote, note);
+                        found = true;
+                        //sn = new VF.GhostNote(n);
+                    }
+                });
+                if(found == false){
                     sn = new VF.GhostNote(n);
                 }
+
             }
             sn.setStyle({fillStyle: noteColors[note[0]], strokeStyle: noteColors[note[0]]});
             let pos_note = parseInt(note[1])-1;
@@ -282,7 +341,9 @@ var Fretboard = function(config) {
                 .append("div")
                 .attr("id", "fretnumbers");
 
+        var allFrets = [];
         for(j=0; j<=instance.frets; j++) {
+            allFrets.push(j);
             let x = j * instance.fretWidth + 1 + instance.XMARGIN();
             instance.svgContainer
                 .append("line")
@@ -293,17 +354,34 @@ var Fretboard = function(config) {
                 .attr("stroke", "lightgray")
                 .attr("stroke-width", j==0? 8:2);
 
-            instance.svgContainer
-                .append("text")
-                .attr("x", function(d) {
-                    let offset = j < 10 ? 33 : 35;
-                    if(j == 0){ offset = 20;}
+        }
+
+        instance.svgContainer
+                .selectAll('text')
+                .data(allFrets)
+                .enter()
+                .append('text').attr("x", function(d, i) {
+                    let x = i * instance.fretWidth + 1 + instance.XMARGIN();
+                    let offset = i < 10 ? 33 : 35;
+                    if(i == 0){ offset = 20;}
                     return x-offset; 
                 })
-                .attr("y", function(d) { return 170; })
-                .text(j);
-
-        }
+                .attr("y", function(d, i) { return 170; })
+                .text(function(d, i){ return i; })
+                .on("mouseover", function(d, i){
+                    d3.selectAll(".note").classed('hidden', true);
+                    //console.log(".fret" + i);
+                    d3.selectAll(".fret" + i).classed('hidden', false);
+                    //renderNotes(notesPerFret[5], instance.notesWidth, instance.frets);
+                    renderNotes(notesPerFret[i], instance.notesWidth, instance.frets);
+                    
+                })
+                .on("mouseout", function(d, i){
+                    d3.selectAll(".note").classed('hidden', false);
+                    renderNotes([], instance.notesWidth, instance.frets);
+                })
+                .attr("cursor", "pointer")
+                ;
     }
 
 
@@ -361,7 +439,7 @@ var Fretboard = function(config) {
 
     // Notes on fretboard
 
-    instance.addNoteOnString = function(note, string, color) {
+    instance.addNoteOnString = function(note, string, color, posFret) {
 
         color = noteColors[note[0]];
 
@@ -381,7 +459,7 @@ var Fretboard = function(config) {
                     var pattern = 'bead';
                     if(note[0] == 'g' || note[0] == 'c' || note[0] == 'f'){ pattern = 'gcf'; }
                     return "note " + note.charAt(0) + " " + note[1] + " " + 
-                           note.charAt(0) + note.charAt(1) + " " + pattern;
+                           note.charAt(0) + note.charAt(1) + " " + pattern + " fret" + posFret;
                 })
                 .attr("stroke-width", 1)
                 // 0.75 is the offset into the fret (higher is closest to fret)
@@ -418,7 +496,7 @@ var Fretboard = function(config) {
                     var pattern = 'bead';
                     if(note[0] == 'g' || note[0] == 'c' || note[0] == 'f'){ pattern = 'gcf'; }
                     return "note " + note.charAt(0) + " " + note[1] + " " + 
-                           note.charAt(0) + note.charAt(1) + " " + pattern;
+                           note.charAt(0) + note.charAt(1) + " " + pattern + " fret" + posFret;
                 })
                 .attr("stroke-width", 1)
                 // 0.75 is the offset into the fret (higher is closest to fret)
@@ -441,7 +519,8 @@ var Fretboard = function(config) {
 
     instance.addNote = function(note, color) {
         for(string=1; string<=instance.strings; string++) {
-            instance.addNoteOnString(note, string, color);
+            var posFret = notesPerString[string + ":" + note];
+            instance.addNoteOnString(note, string, color, posFret);
         }
     };
 
@@ -499,38 +578,54 @@ var Fretboard = function(config) {
         d3.select("#" + id).remove();
     };
 
-    //instance.draw();
-
+    var beadNotes = [
+        ['b', 2], ['b', 3], ['b', 4], ['b', 5], ['b', 6],
+        ['e', 2], ['e', 3], ['e', 4], ['e', 5], ['e', 6], ['e', 7],
+        ['a', 2], ['a', 3], ['a', 4], ['a', 5], ['a', 6],
+        ['d', 2], ['d', 3], ['d', 4], ['d', 5], ['d', 6], ['d', 7]
+    ];
+    var gcfNotes = [
+        ['g', 2], ['g', 3], ['g', 4], ['g', 5], ['g', 6],
+        ['c', 2], ['c', 3], ['c', 4], ['c', 5], ['c', 6], ['c', 7],
+        ['f', 2], ['f', 3], ['f', 4], ['f', 5], ['f', 6]
+    ]
+    
+    d3.select("#bead")
+    .on("mouseover", function(){
+        console.log(instance.fretWidth, instance.frets);
+        d3.selectAll(".note").classed('hidden', true);
+        d3.selectAll(".bead").classed('hidden', false);
+        renderNotes(beadNotes, instance.notesWidth, instance.frets);
+    })
+    .on("mouseout", function(){
+          d3.selectAll(".note").classed('hidden', false);
+          renderNotes([], instance.notesWidth, instance.frets);
+    })
+    .style('cursor', 'pointer');
+    
+    d3.select("#gcf")
+    .on("mouseover", function(){
+          d3.selectAll(".note").classed('hidden', true);
+          d3.selectAll(".gcf").classed('hidden', false);
+          renderNotes(gcfNotes, instance.notesWidth, instance.frets);
+    })
+    .on("mouseout", function(){
+          d3.selectAll(".note").classed('hidden', false);
+          renderNotes([], instance.notesWidth, instance.frets);
+    })
+    .style('cursor', 'pointer');
+    
+    d3.select("#enote")
+    .on("mouseover", function(){
+          d3.selectAll(".note").classed('hidden', true);
+          d3.selectAll(".e").classed('hidden', false);
+    })
+    .on("mouseout", function(){
+          d3.selectAll(".note").classed('hidden', false);
+    })
+    .style('cursor', 'pointer');
 
     return instance;
 };
 
-d3.select("#bead")
-.on("mouseover", function(){
-      d3.selectAll(".note").classed('hidden', true);
-      d3.selectAll(".bead").classed('hidden', false);
-})
-.on("mouseout", function(){
-      d3.selectAll(".note").classed('hidden', false);
-})
-.style('cursor', 'pointer');
 
-d3.select("#gcf")
-.on("mouseover", function(){
-      d3.selectAll(".note").classed('hidden', true);
-      d3.selectAll(".gcf").classed('hidden', false);
-})
-.on("mouseout", function(){
-      d3.selectAll(".note").classed('hidden', false);
-})
-.style('cursor', 'pointer');
-
-d3.select("#enote")
-.on("mouseover", function(){
-      d3.selectAll(".note").classed('hidden', true);
-      d3.selectAll(".e").classed('hidden', false);
-})
-.on("mouseout", function(){
-      d3.selectAll(".note").classed('hidden', false);
-})
-.style('cursor', 'pointer');
